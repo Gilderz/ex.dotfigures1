82b8c53b0c2349a2e87d59c0437d68d6a01a1363
Gilderz <123330752+Gilderz@users.noreply.github.com>
2023-06-19 23:16:02 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+//******Задача про вкладені цикли  (базові графічні примітиви 3)******//
+		
+package main;
+public class Main {
+    public static void main(String[] args) {
+        int rows = 6;
+      //******Трикутник******//
+        for (int i = 0; i < rows; i++) {
+            
+            for (int j = 0; j < 2 * rows - 1; j++) {
+                
+                if (j >= rows - i - 1 && j <= rows + i - 1) {
+                    System.out.print(". ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(); 
+        }
+        System.out.println();
+        //******Трапеція******//
+        	for (int i = 3; i < rows; i++) {
+            
+            for (int j = 0; j < 2 * rows - 1; j++) {
+                
+                if (j >= rows - i - 1 && j <= rows + i - 1) {
+                    System.out.print(". ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(); 
+        }
+    }
+}
